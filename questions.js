2bd6b5fcabcf5beb1fde42793953784988ff8a32
Ade Nova <nttnova3@gmail.com>
2024-05-20 19:47:33 +0700

    attechment 70%

--- questions.js
+++ questions.js
@@ -5,277 +5,133 @@ const questions = [
       { text: "Paris", correct: true },
       { text: "London", correct: false },
       { text: "Berlin", correct: false },
-      { text: "Madrid", correct: false }
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
     ]
   },
   {
-    question: "What is 2 + 2?",
+    question: "What is the capital of France?",
     answers: [
-      { text: "4", correct: true },
-      { text: "3", correct: false },
-      { text: "5", correct: false },
-      { text: "6", correct: false }
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
     ]
   },
   {
-    question: "What is the capital of Japannn?",
+    question: "What is the capital of France?",
     answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
     ]
   },
   {
-    question: "What is the capital of Japan?",
+    question: "What is the capital of France?",
     answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
     ]
   },
   {
-    question: "What is the capital of Japan?",
+    question: "What is the capital of France?",
     answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
+    ]
+  }, {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", correct: true },
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem! Lorem ipsum dolor sit amet consectetur adipisicing elit.Blanditiis, eum voluptates! Ex modi voluptatibus voluptatem excepturi doloremque asperiores quasi dolorem!", correct: false
+      }
     ]
   },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of Japan?",
-    answers: [
-      { text: "Tokyo", correct: true },
-      { text: "Seoul", correct: false },
-      { text: "Beijing", correct: false },
-      { text: "Bangkok", correct: false }
-    ]
-  },
-  {
-    question: "What is 10 / 2?",
-    answers: [
-      { text: "5", correct: true },
-      { text: "4", correct: false },
-      { text: "6", correct: false },
-      { text: "7", correct: false }
-    ]
-  }
 ];
