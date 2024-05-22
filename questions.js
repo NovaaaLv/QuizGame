826f0ecf08312628999cd1128a95cb068ac5e3ca
Nazbro <122434356+Najmuzahir1147@users.noreply.github.com>
2024-05-22 07:31:17 +0700

    questions

--- questions.js
+++ questions.js
@@ -134,6 +134,106 @@ const questions = [
     ]
   },
   {
+    question: "Changes in the earth’s atmosphere took place because of the following except…",
+    answers: [
+      { text: "The changing composition of gasses", correct: false },
+      { text: "The development of plant life", correct: false },
+      { text: "The earth’s original atmosphere", correct: true },
+      { text: "The process of photosynthesis", correct: false },
+    ]
+  },
+  {
+    question: "The topic of the passage is…",
+    answers: [
+      { text: "The static atmosphere", correct: false },
+      { text: "The change in the atmosphere", correct: false },
+      { text: "The earth’s original atmosphere", correct: true },
+      { text: "The process of photosynthesis", correct: false },
+    ]
+  },
+  {
+    question: "Judul bacaan tiluhur nyaéta . . .",
+    answers: [
+      { text: "Raja Pajajaran", correct: false },
+      { text: "Prabu Siliwangi", correct: false },
+      { text: "Mundinglaya Dikusumah", correct: true },
+      { text: "Déwi Patmawati", correct: false }
+    ]
+  },
+  {
+    question: "Kusabab sok ngawurukan ngaji, kang Arip sok disebut . . .",
+    answers: [
+      { text: "Wali", correct: false },
+      { text: "Ustadz", correct: true },
+      { text: "Kepala SD", correct: false },
+      { text: "Nabi", correct: false }
+    ]
+  },
+  {
+    question: "Saha Prabu Siliwangi téh . . .",
+    answers: [
+      { text: "Raja Pajajaran", correct: true },
+      { text: "Raja Singosari", correct: false },
+      { text: "Raja Muara Beres", correct: false },
+      { text: "Raja Kediri", correct: false }
+    ]
+  },
+  {
+    question: "Jenenganana garwa Prabu Siliwangi nyaéta . . .",
+    answers: [
+      { text: "Déwi Padmawati", correct: true },
+      { text: "Déwi Sri", correct: false },
+      { text: "Déwi Banowati", correct: false },
+      { text: "Nyi Gambir Wangi", correct: false }
+    ]
+  },
+  {
+    question: "Kacelik ka awan-awan …. ka jona pria. (kecap rundayan anu marenah pikeun ngeusian titik nyaéta)",
+    answers: [
+      { text: "Kalimpudan", correct: false },
+      { text: "Kahieuman", correct: false },
+      { text: "Kakoncara", correct: true },
+      { text: "Katutupan", correct: false }
+    ]
+  },
+  {
+    question: "Buah Semangka disebut buat kendi, sebab . . .",
+    answers: [
+      { text: "Bauna seungit", correct: false },
+      { text: "Sikina bisa dijien kuaci", correct: false },
+      { text: "Kandungan caina", correct: true },
+      { text: "koneng umyang", correct: false }
+    ]
+  },
+  {
+    question: "Pupuh wiringrong diwangun ku . . .",
+    answers: [
+      { text: "7", correct: false },
+      { text: "8", correct: false },
+      { text: "6", correct: true },
+      { text: "5", correct: false }
+    ]
+  },
+  {
+    question: "Mana kalimah nu ieusina nasihat ?",
+    answers: [
+      { text: "“Cik ujang, Ema pangmeulikeun minyak saléter mah !”", correct: false },
+      { text: "“Urang hirup di dunia téh kudu akur jeung batur “", correct: true },
+      { text: "“Angga budak akuran ka batur”", correct: false },
+      { text: "“Budak éta mah musuhna mani loba”", correct: false }
+    ]
+  },
+  {
+    question: "Pupuh dihandap ieu ngabogaan watek",
+    text: "Pupuh Wirangrong\nDuh ibu hapunten abdi\nRumaos kadalon-dalon\nDisakola teu junun\nDiajarna bari ulin\nTungtungna kénging wiwirang\nTe nana ujian gagal",
+    answers: [
+      { text: "Kadalon-dalon", correct: false },
+      { text: "Sedih", correct: false },
+      { text: "Meunang wiwirang", correct: true },
+      { text: "Kaget éling", correct: false }
+    ]
+  },
+  {
     question: "Pemain bola basket mempunyai tugas pertahanan khusus menjaga satu orang menjaga lawan satu orang adalah ciri sistem pertahanan …",
     answers: [
       { text: "Man to Man Marking", correct: true },
